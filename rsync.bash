993e9c14044449bdf670c3f2a40c887384531690
Stefan <ducdebreme@early-dance.de>
2017-06-05 14:40:56 +0200

    fixed mixed content issue and disabled hint

--- rsync.bash
+++ rsync.bash
@@ -1,3 +1,3 @@
 # rsync.bash
-rsync -zvr ./build/ heurope:/var/www/vhosts/klassikbewegtbinzen.de/httpdocs
+rsync -zvr ./build/ heurope2:/var/www/vhosts/klassikbewegtbinzen.de/httpdocs
 # remote: chown -R klassikbewegt:psaserv .
